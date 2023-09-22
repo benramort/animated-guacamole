825a671d08b8f4128d63f35cd54c1252b9245d57
Beñat Ramirez <benat.ramirez@opendeusto.es>
2023-09-22 09:24:41 +0200

    Commit inicial

---
+++ Prueba I/src/NumerosPares.java
@@ -0,0 +1,14 @@
+public class NumerosPares {
+	
+	public static void main(String[] args) {
+		for (int i = 0; i <= 100; i = i + 2) {
+			System.out.println(i);
+		}
+//		System.out.println(2);
+//		System.out.println(4);
+//		System.out.println(6);
+//		System.out.println(8);
+//		System.out.println(10);
+	}
+
+}
